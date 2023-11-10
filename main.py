c6cf63bd01aac3009549481108f9f477e83d46b6
BrMaro <keremamariari@gmail.com>
2023-11-10 13:36:11 +0300

    added MergeSort

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import time
 import pygame
 import random
-
-WIDTH, HEIGHT = 854, 480
+from BubbleSort import bubble_sort
+from SelectionSort import selection_sort
+from InsertionSort import insertion_sort
+from MergeSort import merge_sort
+WIDTH, HEIGHT = 1200, 900
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Bubble Sort in Action")
 FPS = 100
 
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-RED = (255,0,0)
+RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 PURPLE = (128, 0, 128)
 ORANGE = (255, 165, 0)
@@ -26,6 +30,7 @@ TURQUOISE = (64, 224, 208)
 def is_moved(num):
     colour = RED
 
+
 def gen_array(n):
     array = []
     for _ in range(n):
@@ -33,37 +38,10 @@ def gen_array(n):
     return array
 
 
-def bubble_sort(a):
-    for _ in range(len(a)):
-        for c in range(len(a) - 1):
-            if a[c] > a[c + 1]:
-                a[c], a[c + 1] = a[c + 1], a[c]
-                return a
-
-
-# runs selection sort but makes a change for every run of the game loop not at once
-def selection_sort(a):
-    n = len(a)
-
-    for i in range(n - 1):
-        # Assume the current index is the minimum
-        min_index = i
-
-        # Find the minimum element in the unsorted part of the array
-        for j in range(i + 1, n):
-            if a[j] < a[min_index]:
-                min_index = j
-
-        # Swap the found minimum element with the first element
-        a[i], a[min_index] = a[min_index], a[i]
-        return a
-
-
-
 def draw_window(a):
     WIN.fill(BLACK)
-    pygame.draw.line(WIN, WHITE, (40, HEIGHT - 50), (WIDTH - 30, HEIGHT - 50), 5)
-    pygame.draw.line(WIN, WHITE, (50, 50), (50, HEIGHT - 40), 5)
+    pygame.draw.line(WIN, WHITE, (40, HEIGHT - 50), (WIDTH - 30, HEIGHT - 50), 5)  # y axis
+    pygame.draw.line(WIN, WHITE, (50, 50), (50, HEIGHT - 40), 5)  # x axis
     for number in range(len(a)):
         NUM_BAR = a[number] * 400 // max(a)
         bar_width = 800 // (2 * len(a))
@@ -76,10 +54,10 @@ def draw_window(a):
 
 
 def main():
-    a = gen_array(50)
-
+    a = gen_array(200)
     clock = pygame.time.Clock()
     run = True
+    sorting_generator = selection_sort(a)
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
@@ -87,7 +65,12 @@ def main():
                 run = False
 
         draw_window(a)
-        a = selection_sort(a)
+
+        try:
+            a = next(sorting_generator)
+        except StopIteration:
+            time.sleep(1)
+            run = False
 
     pygame.quit()
 
