a7997bbf433ecdda16f0dfce0b520c72dd2be4a9
BrMaro <keremamariari@gmail.com>
2023-11-10 12:39:04 +0300

    added InsertionSort added SelectionSort

---
+++ InsertionSort.py
@@ -0,0 +1,16 @@
+def insertion_sort(a):
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+
+        while j >= 0 and key < a[j]:
+            a[j + 1] = a[j]
+            j -= 1
+
+        a[j + 1] = key
+        yield a.copy()
+
+
+my_array = [27, 45, 89, 12, 3, 67, 54, 76, 34, 90, 8, 23, 56, 78, 91, 14, 68, 29, 50, 95]
+for i, step in enumerate(insertion_sort(my_array), 1):
+    print(f"Step {i}: {step}")
---
+++ SelectionSort.py
@@ -0,0 +1,18 @@
+# runs selection sort but makes a change for every run of the game loop not at once
+def selection_sort(a):
+    n = len(a)
+
+    for i in range(n - 1):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if a[j] < a[min_index]:
+                min_index = j
+
+        a[i], a[min_index] = a[min_index], a[i]
+        yield a.copy()
+
+
+my_array = [27, 45, 89, 12, 3, 67, 54, 76, 34, 90, 8, 23, 56, 78, 91, 14, 68, 29, 50, 95]
+for i, step in enumerate(selection_sort(my_array), 1):
+    print(f"Step {i}: {step}")
